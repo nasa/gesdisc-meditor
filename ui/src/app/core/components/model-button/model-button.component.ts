1fcc5da836847a867f1f1196089856c51cb6a3e1
Andrey Zasorin <andrey.a.zasorin@nasa.gov>
2018-05-01 16:55:21 -0400

    test for splash page

--- ui/src/app/core/components/model-button/model-button.component.ts
+++ ui/src/app/core/components/model-button/model-button.component.ts
@@ -3,9 +3,18 @@ import { Model } from '../../../service/model/model';
 
 @Component({
 	selector: 'med-model-button',
-	templateUrl: `./model-button.component.html`
+	template: `
+		<button color="primary" (click)="goToSearch.emit(model)" fxFlex="100px"
+			mat-raised-button style="line-height: inherit; padding: 10px;" >
+		  <i class="icon-badge icon-badge-lg fa {{model.icon.name}} align-middle" style="margin-bottom: 5px;" [style.background-color]="model.icon.color"></i>
+		  <div style="font-weight: bold;">{{model.name}}</div>
+		  <div>({{model.count}})</div>
+		</button>
+	`
 })
+
 export class ModelButtonComponent implements OnInit {
+
 	@Input() public model: Model;
 	@Output() goToSearch = new EventEmitter<Model>();
 
