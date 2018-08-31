caba9bbbb0ba34ec89f0a901051301dc68af65da
Andrey Zasorin <andrey.a.zasorin@nasa.gov>
2018-08-31 12:05:44 -0400

    search bar changes

--- ui/src/app/search/components/search-bar/search-bar.component.ts
+++ ui/src/app/search/components/search-bar/search-bar.component.ts
@@ -6,8 +6,8 @@ import {
 	OnInit,
 	OnChanges
 } from '@angular/core';
-import { Model } from '../../../service/model/model';
 import { FormControl } from '@angular/forms';
+import { ModelCatalogEntry } from '../../../service';
 
 @Component({
 	selector: 'med-search-bar',
@@ -15,8 +15,8 @@ import { FormControl } from '@angular/forms';
 	styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-	@Input() models: Model[];
-	@Input() selectedModel: Model;
+	@Input() models: ModelCatalogEntry[];
+	@Input() selectedModel: ModelCatalogEntry;
 	@Output() selectionChanged = new EventEmitter<string>();
 	@Output() searchChanged = new EventEmitter<string>();
 
@@ -28,11 +28,11 @@ export class SearchBarComponent implements OnInit {
 		this.modelControl.setValue({name: this.selectedModel.name, icon: this.selectedModel.icon});
 	}
 
-	ngOnChanges() {
-		if (this.selectedModel) {
-			this.modelControl.setValue({name: this.selectedModel.name, icon: this.selectedModel.icon});
-		}
-	}
+	// ngOnChanges() {
+	// 	if (this.selectedModel) {
+	// 		this.modelControl.setValue({name: this.selectedModel.name, icon: this.selectedModel.icon});
+	// 	}
+	// }
 
 	onSearchChange() {
 		this.searchChanged.emit(this.query);
