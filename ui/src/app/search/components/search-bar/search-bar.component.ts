91223f08ad5a0e76ff3a5bf2198a55d949da6928
Andrey Zasorin <andrey.a.zasorin@nasa.gov>
2018-09-18 10:11:33 -0400

    merge

--- ui/src/app/search/components/search-bar/search-bar.component.ts
+++ ui/src/app/search/components/search-bar/search-bar.component.ts
@@ -3,10 +3,11 @@ import {
 	Input,
 	Output,
 	EventEmitter,
-	OnInit
+	OnInit,
+	OnChanges
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ModelCatalogEntry } from '../../../service';
+import { ModelCatalogEntry } from 'app/service';
 
 @Component({
 	selector: 'med-search-bar',
@@ -20,11 +21,11 @@ export class SearchBarComponent implements OnInit {
 	@Output() searchChanged = new EventEmitter<string>();
 
 	query: string;
-	modelControl: FormControl;
-
+	modelcontrol = new FormControl();
 
 	ngOnInit() {
-		this.modelControl = new FormControl(this.selectedModel);
+		const i  = this.models.map(m => m.name).indexOf(this.selectedModel.name);
+		this.modelcontrol.setValue(this.models[i]);
 	}
 
 	onSearchChange() {
