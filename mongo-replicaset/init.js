14f5ffd683a1968984df95b656a5eb7fe68edf33
Jon Carlson <jdcarlson@gmail.com>
2018-10-10 12:12:46 -0400

    Additional config to connect API

--- mongo-replicaset/init.js
+++ mongo-replicaset/init.js
@@ -1,48 +1,53 @@
-var MongoClient = require('mongodb').MongoClient;
-var Server = require('mongodb').Server;
-var ReplSetServers = require('mongodb').ReplSetServers;
-var MongoUrl = process.env.MONGOURL || "mongodb://meditor_database:27017";
+const MongoClient = require('mongodb').MongoClient
+const Server = require('mongodb').Server
+const ReplSetServers = require('mongodb').ReplSetServers
+const replicaSetConfig = require('./rs-config')
 
-MongoClient.connect(MongoUrl, function(err, db) {
-    if (err) {
-      console.log(err);
-      throw err;
+const MONGO_URL = process.env.MONGOURL || "mongodb://meditor_database:27017"
+const RECONNECT_TIMEOUT_MILLIS = 2000
+const MAX_RECONNECT_ATTEMPTS = 5
+
+async function connectToMongoDb(attempt = 1) {
+    try {
+        return await MongoClient.connect(MONGO_URL)
+    } catch (err) {
+        if (attempt >= MAX_RECONNECT_ATTEMPTS) throw err
+
+        console.log('waiting for mongo to startup...')
+        setTimeout(() => connectToMongoDb(++attempt), RECONNECT_TIMEOUT_MILLIS)
     }
-    var dbo = db.db("meditor");
-    var adminDb = dbo.admin();
-    adminDb.replSetGetStatus(function(err,info){
-        if(err){
-            // Default replica set conf
-            var conf = {
-                "_id" : "meditor",
-                "members" : [
-                    {
-                        "_id" : 0,
-                        "host" : "meditor_database:27017"
-                    },
-                    {
-                        "_id" : 1,
-                        "host" : "meditor_database_sec_1:27017"
-                    },
-                    {
-                        "_id" : 2,
-                        "host" : "meditor_database_sec_2:27017"
-                    }
-                ]
-            };
-  
-            adminDb.command({replSetInitiate: conf}, function(err, info) {
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log(info);
-                }
-                db.close();
-            });
-           
-        }else{
-            console.log("Replica set, "+info.set+", is running");
-            db.close();
-        }
-    });
-}); 
+}
+
+async function getReplicaSetFromDb(db) {
+    try {
+        let adminDb = db.db('meditor').admin()
+        let info = await adminDb.replSetGetStatus()
+        console.log(`Replica set ${info.set} is running`)
+        return info
+    } catch (err) {
+        return null
+    }
+}
+
+async function initializeReplicaSetInDb(db) {
+    try {
+        let adminDb = db.db('meditor').admin()
+        let info = await adminDb.command({ replSetInitiate: replicaSetConfig })
+        console.log(info)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function init() {
+    let db = await connectToMongoDb()
+    let rs = await getReplicaSetFromDb(db)
+
+    if (!rs) {
+        await initializeReplicaSetInDb(db)
+    }
+
+    db.close()
+}
+
+init()
---
+++ mongo-replicaset/rs-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "_id" : "meditor",
+    "members" : [
+        {
+            "_id" : 0,
+            "host" : "meditor_database:27017"
+        },
+        {
+            "_id" : 1,
+            "host" : "meditor_database_sec_1:27017"
+        },
+        {
+            "_id" : 2,
+            "host" : "meditor_database_sec_2:27017"
+        }
+    ]
+}
