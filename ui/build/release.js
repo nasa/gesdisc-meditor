f4f0d23b0333411f11ad4bfdb54231ed92d40662
Jon Carlson <jdcarlson@gmail.com>
2018-10-11 13:39:16 -0400

    Remove console statements

--- ui/build/release.js
+++ ui/build/release.js
@@ -3,13 +3,9 @@ const package = require('../package.json')
 const { bump, git } = require('../node_modules/version-bump-prompt/lib/index')
 
 function init() {
-    console.log('Releasing a new version...')   
-    
     let info = bump('package.json', 'minor', {
         preid: 'beta',
     })
-
-    console.log(info)
 
     git(['package.json'], {
         commit: true,
