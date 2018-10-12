e0ee0299f618af116017137497d2716f8d02e50b
Jon Carlson <jdcarlson@gmail.com>
2018-10-12 10:10:55 -0400

    release 0.3.0

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ version=`cat VERSION`
 echo "export const version = '$version'" >! 'ui/src/environments/version.ts'
 
 # build images
-docker image build -t $REGISTRY/meditor_ui ./ui
-docker image build -t $REGISTRY/meditor_server ./nodejs-server-server
-docker image build -t $REGISTRY/meditor_database ./database
-docker image build -t $REGISTRY/meditor_proxy ./proxy
-docker image build -t $REGISTRY/meditor_notifier ./notifier
+docker image build -t $REGISTRY/meditor_ui ./ui --no-cache
+docker image build -t $REGISTRY/meditor_server ./nodejs-server-server --no-cache
+docker image build -t $REGISTRY/meditor_database ./database --no-cache
+docker image build -t $REGISTRY/meditor_proxy ./proxy --no-cache
+docker image build -t $REGISTRY/meditor_notifier ./notifier --no-cache
