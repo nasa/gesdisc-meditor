878a5ac00c012b221a081014f27e73fb52c7744a
Jon Carlson <jdcarlson@gmail.com>
2018-10-12 10:56:20 -0400

    Make sure version in UI actually gets updated

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ REGISTRY=dev.gesdisc.eosdis.nasa.gov:443
 
 # update visible UI version
 version=`cat VERSION`
-echo "export const version = '$version'" >! 'ui/src/environments/version.ts'
+echo "export const version = '$version'" > './ui/src/environments/version.ts'
 
 # build images
-docker image build -t $REGISTRY/meditor_ui ./ui --no-cache
-docker image build -t $REGISTRY/meditor_server ./nodejs-server-server --no-cache
-docker image build -t $REGISTRY/meditor_database ./database --no-cache
-docker image build -t $REGISTRY/meditor_proxy ./proxy --no-cache
-docker image build -t $REGISTRY/meditor_notifier ./notifier --no-cache
+#docker image build -t $REGISTRY/meditor_ui ./ui --no-cache
+#docker image build -t $REGISTRY/meditor_server ./nodejs-server-server --no-cache
+#docker image build -t $REGISTRY/meditor_database ./database --no-cache
+#docker image build -t $REGISTRY/meditor_proxy ./proxy --no-cache
+#docker image build -t $REGISTRY/meditor_notifier ./notifier --no-cache
