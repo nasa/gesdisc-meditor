71d3b64d63831e18fcd376d5fbc4b217a2b2b10b
Andrey Zasorin <andrey.a.zasorin@nasa.gov>
2018-12-04 12:46:54 -0500

    fix for ckeditor, typescript version updated in package json

--- ui/src/assets/ckeditor/config.js
+++ ui/src/assets/ckeditor/config.js
@@ -1,6 +1,6 @@
 /**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
@@ -35,8 +35,4 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-
-	// mathjaxlib
-	config.mathJaxLib = '/assets/MathJax/unpacked/MathJax.js?config=TeX-AMS_HTML';
-
 };
