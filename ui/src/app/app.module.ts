692630232de2f7f28f90e05069426c0e3d9679d7
Jon Carlson <jdcarlson@gmail.com>
2018-12-05 10:46:27 -0500

    Storage plugin is causing issues, but nothing is ever being stored in LS anyway, so I'm removing it to see if that fixes the errors

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -51,8 +51,6 @@ const routeResolvers = [ DocumentResolver, ModelResolver, DocEditResolver, Model
 			AuthState,
 			WorkflowState ]),
 		NgxsRouterPluginModule.forRoot(),
-    NgxsStoragePluginModule.forRoot({storage: StorageOption.LocalStorage, key: ['auth'], deserialize: JSON.parse,
-      serialize: JSON.stringify}),
 		NgxsReduxDevtoolsPluginModule.forRoot(),
 		ApiModule
 	],
