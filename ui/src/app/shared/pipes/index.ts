e82f8c1a67e17cb76c1561dea3b059ed62d9b66b
Jon Carlson <jdcarlson@gmail.com>
2019-01-08 15:42:29 -0500

    Encode title of document

--- ui/src/app/shared/pipes/index.ts
+++ ui/src/app/shared/pipes/index.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { AddCommasPipe } from './add-commas/add-commas';
 import { EllipsisPipe } from './ellipsis/ellipsis';
+import { EncodeURIComponentPipe } from './encode-uri-component/encode-uri-component'
 
-export const PIPES = [AddCommasPipe, EllipsisPipe];
+export const PIPES = [AddCommasPipe, EllipsisPipe, EncodeURIComponentPipe];
 
 @NgModule({
   declarations: PIPES,
