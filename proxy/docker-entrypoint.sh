8a49367284e359a7fad67eaaca00cd5762569a01
Jon Carlson <jdcarlson@gmail.com>
2019-10-11 16:10:02 -0400

    release 0.54.0

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${SERVER_HOST} ${UI_HOST} ${MONITOR_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${SERVER_HOST} ${UI_HOST} ${MONITOR_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
