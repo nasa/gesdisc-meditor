82f5aa4ac07908501ab03a0470df61a67d623c45
Jon Carlson <jdcarlson@gmail.com>
2020-05-05 09:35:02 -0400

    Redirect old URLs to new

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -8,6 +8,24 @@ import { useState } from 'react'
 import { useRouter } from 'next/router'
 
 /**
+ * mEditor previously used hashes for routes, but hashes aren't passed into the server, so now it's using normal URLs.
+ * Because old URLs may be bookmarked, let's redirect the old URLs to the new URLs
+ */
+const OLD_URL_MAPPING = {
+    '#/search?': { href: '/[modelName]', as: '/{model}' },
+    '#/document/edit?': { href: '/[modelName]/[documentTitle]', as: '/{model}/{title}' },
+    '#/document/new?': { href: '/[modelName]/new', as: '/{model}/new' },
+}
+
+function getOldUrlMapping() {
+    if (typeof window === 'undefined' || !window.location.hash) return
+
+    const oldUrlKey = Object.keys(OLD_URL_MAPPING).find((prefix) => window.location.hash.includes(prefix))
+
+    return oldUrlKey ? OLD_URL_MAPPING[oldUrlKey] : undefined    
+}
+
+/**
  * customize the App to provide a consistent layout across pages
  * @param props.Component the active page
  * @param props.pageProps if page is using getInitialProps, pageProps will contain those
@@ -17,7 +35,25 @@ const App = ({ Component, pageProps }) => {
     const router = useRouter()
 
     const isAuthenticated = typeof user !== 'undefined' && user != null
-    const canLoadPage = typeof user !== 'undefined' || router.pathname == '/'
+    let canLoadPage = typeof user !== 'undefined' || router.pathname == '/'
+
+    const oldUrlMapping = getOldUrlMapping()
+
+    // handle old URLs, use the mapping to construct the new URL
+    // TODO: use mEditor logs to determine if old URLs are still be used, if not, remove this
+    if (oldUrlMapping) {
+        canLoadPage = false
+
+        const hashObj: any = window.location.hash.split('?')[1].split('&').map(v => v.split("=")).reduce( (pre, [key, value]) => ({ ...pre, [key]: value }), {} )
+        
+        let newUrl = oldUrlMapping
+
+        Object.keys(hashObj).forEach(key => newUrl.as = newUrl.as.replace(`{${key}}`, hashObj[key]))
+
+        console.log('using an old URL, redirect to new URL: ', JSON.stringify(newUrl))
+
+        router.push(newUrl.href, newUrl.as)
+    }
    
     return (
         <>
