a5684acadcf4289d6721acaec624650a09d2b206
Jon Carlson <jdcarlson@gmail.com>
2020-05-21 15:55:32 -0400

    Cleanup header

--- ui/components/header.tsx
+++ ui/components/header.tsx
@@ -10,25 +10,21 @@ function goToHomepage() {
     Router.push('/')
 }
 
-const Header = ({
-    user,
-    isAuthenticated,
-}) => {
-    const [ userMenuOpen, setUserMenuOpen ] = useState(false)
+const Header = ({ user, isAuthenticated }) => {
+    const [userMenuOpen, setUserMenuOpen] = useState(false)
 
     return (
         <>
-            <Navbar fixed="top" className={styles.navbar} style={{
-                justifyContent: "space-between",
-                padding: "0 20px",
-            }}>
+            <Navbar
+                fixed="top"
+                className={styles.navbar}
+                style={{
+                    justifyContent: 'space-between',
+                    padding: '0 20px',
+                }}
+            >
                 <Navbar.Brand href="#home" onClick={goToHomepage}>
-                    <img
-                        alt="mEditor"
-                        src="/logo.png"
-                        width="156"
-                        className="d-inline-block align-top"
-                    />
+                    <img alt="mEditor" src="/logo.png" width="156" className="d-inline-block align-top" />
                 </Navbar.Brand>
 
                 <div className="d-flex flex-row">
@@ -38,11 +34,11 @@ const Header = ({
                             onMouseLeave={() => setUserMenuOpen(false)}
                             show={userMenuOpen}
                         >
-                            <Dropdown.Toggle 
-                                className="d-flex align-items-center" 
-                                variant="link" 
+                            <Dropdown.Toggle
+                                className="d-flex align-items-center"
+                                variant="link"
                                 id="user-menu"
-                                style={{ color: "#607d8b" }}
+                                style={{ color: '#607d8b' }}
                             >
                                 <MdPerson style={{ fontSize: '1.6em' }} />
                                 Hi, {user?.firstName}
@@ -56,17 +52,33 @@ const Header = ({
                         </Dropdown>
                     )}
 
-                    <Button className="d-flex align-items-center" variant="link" style={{ color: "grey", marginLeft: 10 }} onClick={goToHomepage}>
+                    <Button
+                        className="d-flex align-items-center"
+                        variant="link"
+                        style={{ color: 'grey', marginLeft: 10 }}
+                        onClick={goToHomepage}
+                    >
                         <MdHome style={{ fontSize: '1.6em' }} />
                         Home
                     </Button>
 
-                    <Button className="d-flex align-items-center" variant="link" style={{ color: "grey", marginLeft: 10 }}>
+                    <Button
+                        className="d-flex align-items-center"
+                        variant="link"
+                        style={{ color: 'grey', marginLeft: 10 }}
+                        href="mailto:gsfc-uui-dev-disc@lists.nasa.gov"
+                    >
                         <MdFeedback style={{ fontSize: '1.6em' }} />
                         Feedback
                     </Button>
 
-                    <Button className="d-flex align-items-center" variant="link" style={{ color: "grey", marginLeft: 10 }}>
+                    <Button
+                        className="d-flex align-items-center"
+                        variant="link"
+                        style={{ color: 'grey', marginLeft: 10 }}
+                        href="https://docs.google.com/document/d/e/2PACX-1vSNC0fvXJ6rbOuuOcAGarA1s4ys3l0mKZ608RgPPlHVvBAYAktxoUWUIsVZqY_QQYN4OvPR6xppz7mI/pub"
+                        target="_blank"
+                    >
                         <MdHelp style={{ fontSize: '1.6em' }} />
                         Help
                     </Button>
