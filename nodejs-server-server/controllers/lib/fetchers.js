2457af48015a0404d975932ee427ec669f5ea866
Jon Carlson <jdcarlson@gmail.com>
2020-07-23 15:52:33 -0400

    release 0.77.0

--- nodejs-server-server/controllers/lib/fetchers.js
+++ nodejs-server-server/controllers/lib/fetchers.js
@@ -9,41 +9,16 @@ var UUI_HEADERS = { // A minimal viable set of UUI headeres
 
 // Retrieves a list of dataset IDs from UUI
 function fetchDatasets() {
-    // Note: can also fetch/transform from here https://cmr.earthdata.nasa.gov/search/collections.umm-json?provider=ges_disc&pretty=true&page_size=2000
-    return requests.post({
-        url: UUI_APP_URL + '/service/datasets/jsonwsp',
-        headers: UUI_HEADERS,
-        followAllRedirects: true,
-        gzip: true,
-        json: true,
-        body: {"methodname":"search","args":{"role":"subset","fields":["dataset.id"]},"type":"jsonwsp/request","version":"1.0"}
+    return new Promise((resolve) => {
+        resolve(['fake', 'tags'])
     })
-    .then(function(res) {
-        return res.result.items.map(item => item.dataset.id);
-    })
-    .catch(function(e) {
-        console.log('Error while trying to fetch UUI datasets from ', UUI_APP_URL + '/service/datasets/jsonwsp', ':', e.message);
-        return ['Failed to fetch the list'];
-    });;
 };
 
 // Retrieves a list of keywords from UUI
 function fetchKeywords() {
-    return requests.post({
-        url: UUI_APP_URL + '/service/keywords/jsonwsp',
-        headers: UUI_HEADERS,
-        followAllRedirects: true,
-        gzip: true,
-        json: true,
-        body: {"methodname": "getKeywords", "args":{"role":"subset"},"type":"jsonwsp/request","version":"1.0"}
+    return new Promise((resolve) => {
+        resolve(['fake', 'tags'])
     })
-    .then(function(res) {
-        return res.result.items;
-    })
-    .catch(function(e) {
-        console.log('Error while trying to fetch UUI keywords from ', UUI_APP_URL + '/service/datasets/jsonwsp', ':', e.message);
-        return ['Failed to fetch the list'];
-    });
 };
 
 module.exports.getFetchers = function() {
