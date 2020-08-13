39ff91fafe655c587c61a335211a69883cd16acd
ajlee8 <amber.j.lee@nasa.gov>
2020-08-13 16:46:52 -0400

    working mongodb instance

---
+++ mongo-replicaset-setup/replicaSet.js
@@ -0,0 +1,1 @@
+rs.initiate( { _id: "rs", members: [ { _id: 1, host: "meditor_mongo-1:27017" }, { _id: 2, host: "meditor_mongo-2:27017" }, { _id: 3, host: "meditor_mongo-3:27017" }, { _id: 4, host: "meditor_mongo-4:27017" } ] } )
