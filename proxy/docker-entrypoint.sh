05c70401af229ea0c595c7fb7c142a0dcee21fdd
Jon Carlson <jdcarlson@gmail.com>
2020-08-13 13:12:16 -0400

    First pass at fixing proxy for windows machines

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${SERVER_HOST} ${UI_HOST} ${MONITOR_HOST} ${NATS_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+if [[ -n "${SERVER_HOST}" ]]; then
+    envsubst '${SERVER_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template
+fi
 
-exec "$@"
+if [[ -n "${UI_HOST}" ]]; then
+    envsubst '${UI_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template
+fi
+
+if [[ -n "${NATS_HOST}" ]]; then
+    envsubst '${NATS_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template
+fi
+
+if [[ -n "${MONITOR_HOST}" ]]; then
+    envsubst '${MONITOR_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template
+fi
+
+cp /etc/nginx/nginx.conf.template /etc/nginx/nginx.conf
+nginx -g daemon off;
