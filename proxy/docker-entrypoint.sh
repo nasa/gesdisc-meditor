189bbc518f2601bb32e26266fa1b7d20ca00d2ba
Jon Carlson <jdcarlson@gmail.com>
2020-08-27 16:13:35 -0400

    Attempt at fixing the proxy for Windows  machines...

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-#SERVER_HOST="${SERVER_HOST:-meditor_server}" \
-#UI_HOST="${UI_HOST:-meditor_ui}" \
-#NATS_HOST="${NATS_HOST:-meditor_nats}" \
-#MONITOR_HOST="${MONITOR_HOST:-meditor_monitor}" \
-#envsubst '${SERVER_HOST} ${UI_HOST} ${NATS_HOST} ${MONITOR_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf;
+SERVER_HOST="${SERVER_HOST:-meditor_server}" \
+UI_HOST="${UI_HOST:-meditor_ui}" \
+NATS_HOST="${NATS_HOST:-meditor_nats}" \
+MONITOR_HOST="${MONITOR_HOST:-meditor_monitor}" \
+envsubst '${SERVER_HOST} ${UI_HOST} ${NATS_HOST} ${MONITOR_HOST}' < /nginx.conf.template > /etc/nginx/nginx.conf
 
-nginx -g 'daemon off;';
+exec "$@"
