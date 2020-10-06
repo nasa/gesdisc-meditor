219d5100d96aebb8c1f498463bb4264e69c64963
Jon Carlson <jdcarlson@gmail.com>
2020-10-06 11:40:15 -0400

    Update to use React component and bind events

--- ui/components/document/source-dialog.tsx
+++ ui/components/document/source-dialog.tsx
@@ -2,43 +2,80 @@ import Card from 'react-bootstrap/Card'
 import React from 'react'
 import styles from './source-dialog.module.css'
 import Button from 'react-bootstrap/Button'
-import { useState} from 'react'
+import { useState, useEffect } from 'react'
+import CodeEditor from '../code-editor'
 
-const SourceDialog = ({ source, title, onSave}) => {
-    const [newSource, setNewSource] = useState(() => {
-        if(!source){
-            return {}
-        }
-        let newSource = source
-        delete newSource['x-meditor']
-        delete newSource._id
-        return newSource
-    })
-    
-    /*
+function isValidJSON(str) {
+    try {
+        JSON.parse(str)
+    } catch (e) {
+        return false
+    }
+    return true
+}
+
+const SourceDialog = ({ source, title, onSave }) => {
+    const [newSource, setNewSource] = useState({})
+    const [validJson, setValidJson] = useState(true)            // used for disabling the save button if user typed in invalid JSON
+
+    // anytime the source changes, update our internal state
     useEffect(() => {
-        setNewSource(source.text)
-    },[source])*/
+        if (source) {
+            let newSource = source
+            delete newSource['x-meditor']
+            delete newSource._id
+            setNewSource(newSource)
+        }
+    }, [source])
 
     const handleClick = (event) => {
         event.preventDefault()
         onSave(newSource)
-        console.log(newSource);
+    }
+
+    const handleSourceChange = (source) => {
+        setValidJson(true)
+
+        if (!source) {
+            return source
+        }
+
+        if (typeof source !== 'string') {
+            setNewSource(source)
+            return
+        }
+
+        // need to make sure JSON is valid before allowing the user to save it
+        if (!isValidJSON(source)) {
+            setValidJson(false)
+            return
+        }
+
+        setNewSource(JSON.parse(source))
     }
 
     return (
         <div>
             <Card className={styles.card}>
                 <Card.Body>
-                    <code-editor
-                        text={JSON.stringify(source, null, 2)}
-                        style={{ width: '350px', height: '400px', display: 'block' }}
-                        onTextChange={(source) => setNewSource(source)}
-                        //onChange={(e) => setNewSource(e.target.value)}
-                    />
-                    <div>
-                    <Button className={styles.button} variant="secondary" onClick={handleClick}>Save</Button>
-                    </div>
+                    {source && <>
+                        <CodeEditor
+                            text={source ? JSON.stringify(source, null, 2) : ""}
+                            style={{ width: '350px', height: '400px', display: 'block' }}
+                            onTextChange={handleSourceChange}
+                        />
+
+                        <div>
+                            <Button
+                                className={styles.button}
+                                variant="secondary"
+                                onClick={handleClick}
+                                disabled={!validJson}
+                            >
+                                Save
+                            </Button>
+                        </div>
+                    </>}
                 </Card.Body>
             </Card>
         </div>
