b183bf394be17bd9f60edfc5f7334f7487f08598
Krupa Rami <krupa.m.rami@nasa.gov>
2021-04-02 16:33:55 -0400

    added usestate and useeffect to show rnd component

--- ui/components/document/document-panel.tsx
+++ ui/components/document/document-panel.tsx
@@ -2,9 +2,25 @@ import styles from './document-panel.module.css'
 import IconButton from '../icon-button'
 import { MdClose } from 'react-icons/md'
 import { Rnd } from 'react-rnd'
+import { useState, useEffect } from 'react'
 
 
 const DocumentPanel = ({ title, children, onClose, open = false, large = false }) => {
+    const [showRnd , setShowRnd] = useState(false)
+    
+    console.log("open ", open)
+    console.log("showRnd ", showRnd)
+    useEffect (() => {
+        if(open) {
+            setShowRnd(true)
+        }else {
+            setShowRnd(false)
+        }
+    },[open])
+
+    //if(showRnd) style.display = "none"
+    //style={showRnd ? null : { display: "none"}}
+
     return (
         <Rnd 
         default={{
@@ -14,7 +30,8 @@ const DocumentPanel = ({ title, children, onClose, open = false, large = false }
         height: 800 ,
       }}
       disableDragging
-      //style={{display: "show" && "none"}}
+      style={{display: showRnd ? "block" : "none"}}
+     // show={showRnd}
       >
         <div className={`${styles.panel} ${open ? styles.panelOpen : ''} ${large ? styles.panelLarge : ''}`}>
           
