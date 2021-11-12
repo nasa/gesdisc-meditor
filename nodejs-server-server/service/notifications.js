133839c9c262116b589f0dec7ec2dad0405331c6
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2021-11-12 16:36:13 -0500

    Add tests for retrieving target roles

--- nodejs-server-server/service/notifications.js
+++ nodejs-server-server/service/notifications.js
@@ -1,14 +1,34 @@
 const { onlyUnique } = require("../utils/array");
 
 /**
- * given a workflow and a state, get a list of edges that the document can move into
+ * given a list of edges and a state, will return a list of roles that can transition from this state
+ *
+ * @param {*} edges
+ * @param {*} state
+ * @returns
+ */
+function getTargetRoles(edges, state) {
+  // roles are based on edges, so get edges that branch from the state
+  const targetEdges = getTargetEdges(edges, state);
+
+  // return list of unique roles
+  return targetEdges.map((edge) => edge.role).filter(onlyUnique);
+}
+
+/**
+ * given a list of edges and a state, get a list of edges that the document can move into
  * ex. a document in "Under Review" state could have target edges: ["Approve", "Reject"]
  *
- * @param {*} workflow
- * @param {*} currentState
+ * @param {*} edges
+ * @param {*} state
  */
-module.exports.getTargetEdges = function (workflow, currentState) {
-  return workflow.edges
-    .filter((edge) => edge.source === currentState) // retrieve edges that branch off the current state
+function getTargetEdges(edges, state) {
+  return edges
+    .filter((edge) => edge.source === state) // retrieve edges that branch off the current state
     .filter(onlyUnique); // remove duplicates
+}
+
+module.exports = {
+  getTargetEdges,
+  getTargetRoles,
 };
--- nodejs-server-server/service/notifications.test.js
+++ nodejs-server-server/service/notifications.test.js
@@ -1,24 +1,54 @@
-const { getTargetEdges } = require("./notifications");
+const { getTargetEdges, getTargetRoles } = require("./notifications");
 const modifyReviewPublishWorkflow = require("./__test__/modify-review-publish.workflow.json");
+
+describe("getTargetRoles", () => {
+  it("returns empty array for invalid state", () => {
+    expect(getTargetRoles(modifyReviewPublishWorkflow.edges, "Foo")).toEqual(
+      []
+    );
+  });
+
+  it("returns roles that can transition from a state", () => {
+    expect(
+      getTargetRoles(modifyReviewPublishWorkflow.edges, "Under Review")
+    ).toEqual(["Reviewer"]);
+  });
+
+  it('returns roles that can transition from "Published" state', () => {
+    expect(
+      getTargetRoles(modifyReviewPublishWorkflow.edges, "Published")
+    ).toEqual(["Publisher"]);
+  });
+
+  it('returns roles that can transition from "Init" state', () => {
+    expect(getTargetRoles(modifyReviewPublishWorkflow.edges, "Init")).toEqual([
+      "Author",
+    ]);
+  });
+});
 
 describe("getTargetEdges", () => {
   it("returns empty array for invalid state", () => {
-    expect(getTargetEdges(modifyReviewPublishWorkflow, "Foo")).toEqual([]);
+    expect(getTargetEdges(modifyReviewPublishWorkflow.edges, "Foo")).toEqual(
+      []
+    );
   });
 
   it("returns first edge for initial state", () => {
-    expect(getTargetEdges(modifyReviewPublishWorkflow, "Init")).toEqual([
+    expect(getTargetEdges(modifyReviewPublishWorkflow.edges, "Init")).toEqual([
       modifyReviewPublishWorkflow.edges[0],
     ]);
   });
 
   it("returns empty array for final/end state", () => {
-    expect(getTargetEdges(modifyReviewPublishWorkflow, "Hidden")).toEqual([]);
+    expect(getTargetEdges(modifyReviewPublishWorkflow.edges, "Hidden")).toEqual(
+      []
+    );
   });
 
   it("returns applicable edges for inner workflow state", () => {
     let targetEdges = getTargetEdges(
-      modifyReviewPublishWorkflow,
+      modifyReviewPublishWorkflow.edges,
       "Under Review"
     );
 
