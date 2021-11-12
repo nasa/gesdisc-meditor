6721794cf77a4278fe66c325fa0758f1ecf094f2
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2021-11-12 16:49:23 -0500

    Fix formatting

--- nodejs-server-server/service/notifications.test.js
+++ nodejs-server-server/service/notifications.test.js
@@ -1,67 +1,67 @@
-const { NotificationsService } = require("./notifications");
-const modifyReviewPublishWorkflow = require("./__test__/modify-review-publish.workflow.json");
+const { NotificationsService } = require('./notifications')
+const modifyReviewPublishWorkflow = require('./__test__/modify-review-publish.workflow.json')
 
-describe("NotificationsService", () => {
-  let notifications = new NotificationsService();
+describe('NotificationsService', () => {
+    let notifications = new NotificationsService()
 
-  it("getTargetRoles() returns empty array for invalid state", () => {
-    expect(
-      notifications.getTargetRoles(modifyReviewPublishWorkflow.edges, "Foo")
-    ).toEqual([]);
-  });
+    it('getTargetRoles() returns empty array for invalid state', () => {
+        expect(
+            notifications.getTargetRoles(modifyReviewPublishWorkflow.edges, 'Foo')
+        ).toEqual([])
+    })
 
-  it("getTargetRoles() returns roles that can transition from a state", () => {
-    expect(
-      notifications.getTargetRoles(
-        modifyReviewPublishWorkflow.edges,
-        "Under Review"
-      )
-    ).toEqual(["Reviewer"]);
-  });
+    it('getTargetRoles() returns roles that can transition from a state', () => {
+        expect(
+            notifications.getTargetRoles(
+                modifyReviewPublishWorkflow.edges,
+                'Under Review'
+            )
+        ).toEqual(['Reviewer'])
+    })
 
-  it('getTargetRoles() returns roles that can transition from "Published" state', () => {
-    expect(
-      notifications.getTargetRoles(
-        modifyReviewPublishWorkflow.edges,
-        "Published"
-      )
-    ).toEqual(["Publisher"]);
-  });
+    it('getTargetRoles() returns roles that can transition from "Published" state', () => {
+        expect(
+            notifications.getTargetRoles(
+                modifyReviewPublishWorkflow.edges,
+                'Published'
+            )
+        ).toEqual(['Publisher'])
+    })
 
-  it('getTargetRoles() returns roles that can transition from "Init" state', () => {
-    expect(
-      notifications.getTargetRoles(modifyReviewPublishWorkflow.edges, "Init")
-    ).toEqual(["Author"]);
-  });
+    it('getTargetRoles() returns roles that can transition from "Init" state', () => {
+        expect(
+            notifications.getTargetRoles(modifyReviewPublishWorkflow.edges, 'Init')
+        ).toEqual(['Author'])
+    })
 
-  it("getTargetEdges() returns empty array for invalid state", () => {
-    expect(
-      notifications.getTargetEdges(modifyReviewPublishWorkflow.edges, "Foo")
-    ).toEqual([]);
-  });
+    it('getTargetEdges() returns empty array for invalid state', () => {
+        expect(
+            notifications.getTargetEdges(modifyReviewPublishWorkflow.edges, 'Foo')
+        ).toEqual([])
+    })
 
-  it("getTargetEdges() returns first edge for initial state", () => {
-    expect(
-      notifications.getTargetEdges(modifyReviewPublishWorkflow.edges, "Init")
-    ).toEqual([modifyReviewPublishWorkflow.edges[0]]);
-  });
+    it('getTargetEdges() returns first edge for initial state', () => {
+        expect(
+            notifications.getTargetEdges(modifyReviewPublishWorkflow.edges, 'Init')
+        ).toEqual([modifyReviewPublishWorkflow.edges[0]])
+    })
 
-  it("getTargetEdges() returns empty array for final/end state", () => {
-    expect(
-      notifications.getTargetEdges(modifyReviewPublishWorkflow.edges, "Hidden")
-    ).toEqual([]);
-  });
+    it('getTargetEdges() returns empty array for final/end state', () => {
+        expect(
+            notifications.getTargetEdges(modifyReviewPublishWorkflow.edges, 'Hidden')
+        ).toEqual([])
+    })
 
-  it("getTargetEdges() returns applicable edges for inner workflow state", () => {
-    let targetEdges = notifications.getTargetEdges(
-      modifyReviewPublishWorkflow.edges,
-      "Under Review"
-    );
+    it('getTargetEdges() returns applicable edges for inner workflow state', () => {
+        let targetEdges = notifications.getTargetEdges(
+            modifyReviewPublishWorkflow.edges,
+            'Under Review'
+        )
 
-    // just check the labels for equality
-    expect(targetEdges.map((edge) => edge.label)).toEqual([
-      "Needs more work",
-      "Approve publication",
-    ]);
-  });
-});
+        // just check the labels for equality
+        expect(targetEdges.map(edge => edge.label)).toEqual([
+            'Needs more work',
+            'Approve publication',
+        ])
+    })
+})
