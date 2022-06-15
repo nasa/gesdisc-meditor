f13062f5f997b9b4b4c88353df35ab052050e9a0
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2022-06-15 16:54:43 -0400

    Remove redundant sort

--- ui/utils/sort.ts
+++ ui/utils/sort.ts
@@ -4,8 +4,5 @@ export function sortModels(modelA: Model, modelB: Model) {
     if (modelA.category < modelB.category) return 1
     if (modelA.category > modelB.category) return -1
 
-    if (modelA.category < modelB.category) return -1
-    if (modelA.category > modelB.category) return 1
-
     return 0
 }
