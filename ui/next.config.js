ba7933dd731a9ad9fb6227515d1aac0164ff29ca
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2022-06-24 15:39:23 -0400

    Add redirects for old routes

--- ui/next.config.js
+++ ui/next.config.js
@@ -8,4 +8,24 @@ module.exports = {
         }
         return config
     },
+    async redirects() {
+        return [
+            {
+                source: '/api/listModels',
+                permanent: true,
+                destination: '/api/models',
+            },
+            {
+                source: '/api/listDocuments',
+                has: [
+                    {
+                        type: 'query',
+                        key: 'model',
+                    },
+                ],
+                permanent: true,
+                destination: '/api/models/:model/documents',
+            },
+        ]
+    },
 }
