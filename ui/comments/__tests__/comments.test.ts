e7a22185c2b8d11144559109914bee9eca8835d8
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2022-10-11 09:33:21 -0400

    Move existing tests

--- ui/comments/__tests__/comments.test.ts
+++ ui/comments/__tests__/comments.test.ts
@@ -1,8 +1,13 @@
 import { Db } from 'mongodb'
 import getDb from '../../lib/mongodb'
 import alertsModel from '../../models/__test__/fixtures/models/alerts.json'
-import { getCommentForDocument, getCommentsForDocument } from '../service'
+import {
+    getCommentForDocument,
+    getCommentsForDocument,
+    createCommentAsUser,
+} from '../service'
 import mockComments from './__fixtures__/comments.json'
+import BaconUser from '../../auth/__test__/__fixtures__/bacon-user.json'
 
 const mockAlerts = [
     {
@@ -28,6 +33,7 @@ describe('Comments Service', () => {
         await db.collection('Comments').deleteMany({})
     })
 
+    /*
     test('collection return all comments for an existing model and document with comments', async () => {
         const comments = await getCommentsForDocument({
             documentTitle: 'Mock Alert w/ Comments & Troublesome Title',
@@ -49,17 +55,16 @@ describe('Comments Service', () => {
     })
 
     test('singleton returns one comment for an existing model and document with comments', async () => {
-        const comments = await getCommentsForDocument({
+        const [error, comments] = await getCommentsForDocument({
             documentTitle: 'Mock Alert w/ Comments & Troublesome Title',
             modelName: 'Alerts',
         })
 
-        const [firstComment] = comments
         const {
             _id: commentId,
             documentId: documentTitle,
             model: modelName,
-        } = firstComment
+        } = comments[0]
 
         const comment = await getCommentForDocument({
             commentId,
@@ -80,5 +85,78 @@ describe('Comments Service', () => {
 
         expect(comment).toHaveLength(0)
         expect(comment).toMatchSnapshot()
+    })*/
+
+    it('throws an UnauthorizedException if the user is logged out', async () => {
+        await expect(async () =>
+            createCommentAsUser(
+                {
+                    model: 'Foo',
+                    documentId: 'Bar',
+                    text: 'Testing unresolved comment by default',
+                },
+                {} as any // force logged out
+            )
+        ).rejects.toThrowErrorMatchingInlineSnapshot(`"Unauthorized"`)
+    })
+
+    it('throws a BadRequestException if the comment is invalid', async () => {
+        await expect(async () => createCommentAsUser({} as any, BaconUser)).rejects
+            .toThrowErrorMatchingInlineSnapshot(`
+                    "0: instance requires property \\"documentId\\"
+                    1: instance requires property \\"model\\"
+                    2: instance requires property \\"text\\"
+                    "
+                `)
+    })
+
+    it('creates an unresolved comment by default', async () => {
+        const newComment = await createCommentAsUser(
+            {
+                model: 'Foo',
+                documentId: 'Bar',
+                text: 'Testing unresolved comment by default',
+            },
+            BaconUser
+        )
+
+        expect(newComment.resolved).toEqual(false)
+    })
+
+    it('creates a new comment as a root comment by default', async () => {
+        const newComment = await createCommentAsUser(
+            {
+                model: 'Foo',
+                documentId: 'Bar',
+                text: 'Testing unresolved comment by default',
+            },
+            BaconUser
+        )
+
+        expect(newComment.parentId).toEqual('root')
+    })
+
+    it('creates a comment on a document', async () => {
+        const { _id, createdOn, ...newComment } = await createCommentAsUser(
+            {
+                model: 'Foo',
+                documentId: 'Bar',
+                text: 'Test comment',
+                parentId: 'foo',
+            },
+            BaconUser
+        )
+
+        expect(newComment).toMatchInlineSnapshot(`
+            Object {
+              "createdBy": "Bacon User",
+              "documentId": "Bar",
+              "model": "Foo",
+              "parentId": "foo",
+              "resolved": false,
+              "text": "Test comment",
+              "userUid": "bacon",
+            }
+        `)
     })
 })
