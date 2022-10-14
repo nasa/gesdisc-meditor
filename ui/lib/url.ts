759a964491a680cb0edd18cf7ee9d43d1b46e4e8
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2022-10-14 13:19:08 -0400

    With NGINX no longer decoding, these encoding functions can be removed

--- ui/lib/url.ts
+++
@@ -1,7 +0,0 @@
-export const urlEncode = (url) => {
-    return encodeURIComponent(url).replace(/%2F/gi, '%252F')
-}
-
-export const urlDecode = (url) => {
-    return decodeURIComponent(url).replace(/%2F/gi, '/')
-}
