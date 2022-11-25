9b15d77f0fa6a3029f9fb728e9274b80912aad40
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2022-11-25 11:06:08 -0500

    Switch to mocking debug so that we can use console.log to help with jest setup

--- ui/jest.setupAfter.ts
+++ ui/jest.setupAfter.ts
@@ -1,10 +1,11 @@
 import { mongoClientPromise } from './lib/mongodb'
 
 // mocks console so when we are testing expected output we don't pollute the Jest output
+// TODO: after a logger is implemented, this should be removed in favor of using a mocking type solution
 global.console = {
     ...console,
     error: jest.fn(),
-    log: jest.fn(),
+    debug: jest.fn(),
 }
 
 process.env.DB_NAME = 'mock-meditor'
