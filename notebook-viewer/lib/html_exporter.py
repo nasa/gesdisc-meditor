a20006b963e9a744abad6786aeedb33ae1ef21e2
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2022-12-22 11:22:12 -0500

    Semantic tag names

--- notebook-viewer/lib/html_exporter.py
+++ notebook-viewer/lib/html_exporter.py
@@ -18,8 +18,8 @@ dl = DictLoader(
 <body class="jp-Notebook" data-jp-theme-light="true" data-jp-theme-name="JupyterLab Light">
 {% endif %}
 
-<div style="display: flex; justify-content: flex-end; align-items: center;">
-    <div>
+<header style="display: flex; justify-content: flex-end; align-items: center;">
+    <nav>
         <a href="{{ resources.notebookUrl }}" title="View Notebook as Code" style="margin-left: 10px;">
             <svg width="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                 <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
@@ -45,8 +45,8 @@ dl = DictLoader(
             </svg>
         </a>
         {% endif %}
-    </div>
-</div>
+    </nav>
+</header>
 {%- endblock body_header -%}
 """
     }
