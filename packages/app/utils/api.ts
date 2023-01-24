310f29a2bd8201ed80ca56478eee8ce743045054
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2023-01-24 09:39:00 -0500

    Don't override status code for noContent calls

--- packages/app/utils/api.ts
+++ packages/app/utils/api.ts
@@ -6,23 +6,25 @@ export function respondAsJson(
     payload: Serializable,
     request: NextApiRequest,
     response: NextApiResponse,
-    options?: {
-        httpStatusCode?: number // to pass a custom HTTP status code
+    options: {
+        httpStatusCode?: number // to pass a custom HTTP status code, defaults to `200`
+    } = {
+        httpStatusCode: 200,
     }
 ) {
     if (request.query.noOutput) {
         // user requested no output, only return the HTTP status code
-        return response.status(204).end()
+        return response.status(options.httpStatusCode).end()
     }
 
     if (request.query.pretty) {
         // user requested the JSON output to be prettified
         response.setHeader('Content-Type', 'application/json')
         return response
-            .status(options?.httpStatusCode || 200)
+            .status(options.httpStatusCode)
             .send(JSON.stringify(payload, null, 2))
     }
 
     // a normal response is minified JSON with the HTTP status code
-    return response.status(options?.httpStatusCode || 200).json(payload)
+    return response.status(options.httpStatusCode).json(payload)
 }
