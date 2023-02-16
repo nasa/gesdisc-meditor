ee4387b606df2ac9bd730d82defd1d9c53e55e9d
Krupa Rami <krupa.m.rami@nasa.gov>
2023-02-16 12:55:11 -0500

    formated document

--- packages/app/pages/api/health/index.ts
+++ packages/app/pages/api/health/index.ts
@@ -13,24 +13,24 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
     const controller = new AbortController();
 
     try {
-    const timeoutId = setTimeout(() => controller.abort(), 5000);
+        const timeoutId = setTimeout(() => controller.abort(), 5000);
 
-    //fetching response from meditor_notifier whether it is healthy or not
-    const response = await fetch(
-        'http://meditor_notifier:3000/health', { signal: controller.signal })
-    clearTimeout(timeoutId);
+        //fetching response from meditor_notifier whether it is healthy or not
+        const response = await fetch(
+            'http://meditor_notifier:3000/health', { signal: controller.signal })
+        clearTimeout(timeoutId);
 
-    const result = await response.json()
+        const result = await response.json()
 
-    healthcheck.services.email_notifier.isHealthy = result.isHealthy
+        healthcheck.services.email_notifier.isHealthy = result.isHealthy
 
-    //To make sure Meditor API it self is healhty
-    res.status(200).send(JSON.stringify(healthcheck, null, 2))
+        //To make sure Meditor API it self is healhty
+        res.status(200).send(JSON.stringify(healthcheck, null, 2))
 
     } catch (err) {
 
         console.error(err);
-        
+
         res.status(500).json({ message: 'Meditor API is not healthy', err })
     }
 }
