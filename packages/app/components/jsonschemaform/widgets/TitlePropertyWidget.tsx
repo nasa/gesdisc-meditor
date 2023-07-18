2ade22280339405f6608424f0fe896323657ad8b
Krupa Rami <krupa.m.rami@nasa.gov>
2023-07-18 16:35:57 -0400

    added titlePropertywidget for trimming white spaces to models

---
+++ packages/app/components/jsonschemaform/widgets/TitlePropertyWidget.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function TitlePropertyWidget(props) {
+    const {
+        id,
+        placeholder,
+        required,
+        disabled,
+        readonly,
+        autofocus,
+        value,
+        onChange,
+        onBlur,
+        onFocus,
+    } = props
+
+    return (
+        <input
+            type="text"
+            className="form-control"
+            id={id}
+            placeholder={placeholder}
+            required={required}
+            disabled={disabled || readonly}
+            autoFocus={autofocus || false}
+            value={value}
+            onBlur={onBlur && (event => onBlur(id, event.target.value.trim()))}
+            onChange={event => onChange(event.target.value)}
+        />
+    )
+}
+
+export default TitlePropertyWidget
