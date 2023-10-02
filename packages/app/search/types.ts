d94471713311162c7d972cda09810ee785bc94d0
Ben Forshey <benjamin.a.forshey@nasa.gov>
2023-10-02 15:48:47 -0400

    chore: clarify types for devs

--- packages/app/search/types.ts
+++ packages/app/search/types.ts
@@ -1,9 +1,14 @@
 export type PaginatedSearchResults = {
     metadata: {
+        /* total count of all pages */
         pageCount: number
+        /* current page number */
         pageNumber: number
+        /* the query as parsed by the search service */
         query: string
+        /* total count of all results */
         resultsCount: number
+        /* number of results per page */
         resultsPerPage: number
     }
     results: any[]
