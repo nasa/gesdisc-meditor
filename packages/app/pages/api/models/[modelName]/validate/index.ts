67d9d394b7dd83fa08553b79a85ba92b2bd5980b
Ben Forshey <benjamin.a.forshey@nasa.gov>
2024-05-24 12:29:13 -0400

    feat(app): add strict schema validation endpoint

---
+++ packages/app/pages/api/models/[modelName]/validate/index.ts
@@ -0,0 +1,63 @@
+import { getLoggedInUser } from 'auth/user'
+import { strictValidateDocument } from 'documents/service'
+import { getModelWithWorkflow, userCanAccessModel } from 'models/service'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { respondAsJson } from 'utils/api'
+import { apiError, ErrorCode, HttpException } from 'utils/errors'
+import { safeParseJSON } from 'utils/json'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const modelName = decodeURIComponent(req.query.modelName.toString())
+    const user = await getLoggedInUser(req, res)
+
+    if (!userCanAccessModel(modelName, user)) {
+        return apiError(
+            new HttpException(
+                ErrorCode.ForbiddenError,
+                'User does not have access to the requested model'
+            ),
+            res
+        )
+    }
+
+    switch (req.method) {
+        //* GET returns the model's schema that will be used to valide the document.
+        case 'GET': {
+            const [modelWithWorkflowError, modelWithWorkflow] =
+                await getModelWithWorkflow(modelName, undefined, {
+                    populateMacroTemplates: true,
+                })
+
+            if (modelWithWorkflowError) {
+                return apiError(modelWithWorkflowError, res)
+            }
+
+            return respondAsJson(JSON.parse(modelWithWorkflow.schema), req, res)
+        }
+
+        //* Unlike most POST endpoints, this allows unauthenticated access.
+        case 'POST': {
+            const [parsingError, parsedDocument] = safeParseJSON(req.body)
+
+            if (parsingError) {
+                return apiError(parsingError, res)
+            }
+
+            const [validationError, validDocument] = await strictValidateDocument(
+                parsedDocument,
+                modelName
+            )
+
+            if (validationError) {
+                return apiError(validationError, res)
+            }
+
+            return respondAsJson(validDocument, req, res, {
+                httpStatusCode: 200,
+            })
+        }
+
+        default:
+            return res.status(405).end()
+    }
+}
