bf4c78353ca808e07ce2b1e139a924816d022c0d
Ben Forshey <benjamin.a.forshey@nasa.gov>
2024-09-30 11:48:32 -0400

    fix(datetimewidget): fix broken onBlur event handling
    @rsjf (or our `onBlur` function) did not correctly handle the onBlur event--the input's value would
    not save to form state. `onBlur` now calls @rsjf's native `onChange` event.
    
    fix #53470

--- packages/app/components/jsonschemaform/widgets/DateTimeWidget.tsx
+++ packages/app/components/jsonschemaform/widgets/DateTimeWidget.tsx
@@ -3,6 +3,23 @@ import Flatpickr from 'react-flatpickr'
 import { format, zonedTimeToUtc } from 'date-fns-tz'
 import type { WidgetProps } from '@rjsf/utils'
 
+function formatDate(date: string, dateFormatOption: any) {
+    let dateValue
+
+    switch (dateFormatOption) {
+        case 'Z':
+            dateValue = zonedTimeToUtc(new Date(date), 'Etc/UTC').toISOString()
+            break
+
+        default:
+            dateValue = format(new Date(date), 'yyyy-MM-dd HH:mm:ssxxx')
+
+            break
+    }
+
+    return dateValue
+}
+
 function DateTimeWidget(props: WidgetProps) {
     const {
         id,
@@ -28,25 +45,15 @@ function DateTimeWidget(props: WidgetProps) {
             disabled={disabled || readonly}
             autoFocus={autofocus || false}
             defaultValue={value}
-            onBlur={() => {
-                onBlur(id, value)
-            }}
-            onChange={(_selectedDates, date) => {
-                let dateValue
-
-                switch (dateFormatOption) {
-                    case 'Z':
-                        dateValue = zonedTimeToUtc(
-                            new Date(date),
-                            'Etc/UTC'
-                        ).toISOString()
-                        break
-
-                    default:
-                        dateValue = format(new Date(date), 'yyyy-MM-dd HH:mm:ssxxx')
+            onBlur={(event: any) => {
+                // @rsjf (or maybe our onBlur function) does not correctly handle the blur event for this component.
+                // Instead of onBlur, call onChange if there's a value.
+                if (event.target.value) {
+                    onChange(formatDate(event.target.value, dateFormatOption))
                 }
-
-                onChange(dateValue)
+            }}
+            onChange={(_selectedDates: any, date: string) => {
+                onChange(formatDate(date, dateFormatOption))
             }}
             options={{
                 time_24hr: true,
