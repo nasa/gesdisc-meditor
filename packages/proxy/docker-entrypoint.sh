1d5a999c774d05ab0a51faa0534a728a40754917
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2024-11-22 16:48:36 -0500

    feature: upgrade NGINX to 1.27.2

--- packages/proxy/docker-entrypoint.sh
+++ packages/proxy/docker-entrypoint.sh
@@ -3,9 +3,8 @@ set -eu
 
 UI_HOST="${UI_HOST:-meditor_app}" \
 DOCS_HOST="${DOCS_HOST:-meditor_docs}" \
-NATS_HOST="${NATS_HOST:-meditor_nats}" \
 MONITOR_HOST="${MONITOR_HOST:-meditor_monitor}" \
 NOTEBOOKVIEWER_HOST="${NOTEBOOKVIEWER_HOST:-meditor_notebookviewer}" \
-envsubst '${UI_HOST} ${DOCS_HOST} ${NATS_HOST} ${MONITOR_HOST} ${NOTEBOOKVIEWER_HOST}' < /nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${UI_HOST} ${DOCS_HOST} ${MONITOR_HOST} ${NOTEBOOKVIEWER_HOST}' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
