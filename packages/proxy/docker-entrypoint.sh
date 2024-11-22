7321ce09fde9e4701977bfda87dbff61074fe465
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2024-11-22 16:14:15 -0500

    feature: remove legacy API from proxy

--- packages/proxy/docker-entrypoint.sh
+++ packages/proxy/docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 set -eu
 
-SERVER_HOST="${SERVER_HOST:-meditor_legacy-api}" \
 UI_HOST="${UI_HOST:-meditor_app}" \
 DOCS_HOST="${DOCS_HOST:-meditor_docs}" \
 NATS_HOST="${NATS_HOST:-meditor_nats}" \
 MONITOR_HOST="${MONITOR_HOST:-meditor_monitor}" \
 NOTEBOOKVIEWER_HOST="${NOTEBOOKVIEWER_HOST:-meditor_notebookviewer}" \
-envsubst '${SERVER_HOST} ${UI_HOST} ${DOCS_HOST} ${NATS_HOST} ${MONITOR_HOST} ${NOTEBOOKVIEWER_HOST}' < /nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${UI_HOST} ${DOCS_HOST} ${NATS_HOST} ${MONITOR_HOST} ${NOTEBOOKVIEWER_HOST}' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
