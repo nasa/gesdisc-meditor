31caa0243b195ca14bbac6482e0fa4917ef5ee03
Ben Forshey <benjamin.a.forshey@nasa.gov>
2024-12-20 10:35:14 -0500

    feat(notebookviewer): upgrade dependencies to resolve ECR scan findings

--- packages/notebook-viewer/app.py
+++ packages/notebook-viewer/app.py
@@ -6,9 +6,12 @@ import re
 from lib.html_exporter import html_exporter
 
 # only notebooks from these domains are allowed
-DOMAIN_WHITELIST_REGEX = "^https://([a-zA-Z0-9]+\.)*(nasa\.gov|github\.com|githubusercontent\.com)\/?.*"
+DOMAIN_WHITELIST_REGEX = (
+    r"^https://([a-zA-Z0-9]+\.)*(nasa\.gov|github\.com|githubusercontent\.com)\/?.*"
+)
 
-app = Flask('mEditor Notebook Viewer')
+app = Flask("mEditor Notebook Viewer")
+
 
 @app.route("/meditor/notebookviewer/")
 def getNotebookAsHtml():
@@ -17,39 +20,43 @@ def getNotebookAsHtml():
 
 def convertNotebookToHtml():
     # ensure the user passed in a notebook url
-    if request.args.get('notebookUrl') == None:
+    if request.args.get("notebookUrl") is None:
         return "Missing a required URL parameter, `notebookUrl`", 400
 
-    notebookUrl = urllib.parse.unquote(request.args.get('notebookUrl'))
+    notebookUrl = urllib.parse.unquote(request.args.get("notebookUrl"))
 
     # ensure the extension is a notebook extension
-    if not notebookUrl.endswith('.ipynb'):
+    if not notebookUrl.endswith(".ipynb"):
         return "URL does not point to a Jupyter Notebook", 400
 
     # ensure the domain is in the whitelist
     if not re.search(DOMAIN_WHITELIST_REGEX, notebookUrl):
         return "We cannot convert a notebook from the provided domain", 400
 
-    githubUrl = ''
-    
+    githubUrl = ""
+
     # if we're including a github.com URL, we'll provide some additional links to the original github repo
-    if notebookUrl.startswith('https://github.com'):
+    if notebookUrl.startswith("https://github.com"):
         # make sure we are only rendering notebooks from the nasa organization
-        if not notebookUrl.startswith('https://github.com/nasa/'):
+        if not notebookUrl.startswith("https://github.com/nasa/"):
             return "Invalid notebook URL, must be in the NASA organization", 400
 
         githubUrl = notebookUrl
         notebookUrl = notebookUrl.replace(
-            "https://github.com", "https://raw.githubusercontent.com").replace("/blob/", "/")
+            "https://github.com", "https://raw.githubusercontent.com"
+        ).replace("/blob/", "/")
 
     # read the notebook content in
     response = urlopen(notebookUrl).read().decode()
 
     # convert it to HTML
     notebook = nbformat.reads(response, as_version=4)
-    (body, resources) = html_exporter.from_notebook_node(notebook, resources={
-        'notebookUrl': notebookUrl,
-        'githubUrl': githubUrl,
-    })
+    (body, _resources) = html_exporter.from_notebook_node(
+        notebook,
+        resources={
+            "notebookUrl": notebookUrl,
+            "githubUrl": githubUrl,
+        },
+    )
 
     return body
